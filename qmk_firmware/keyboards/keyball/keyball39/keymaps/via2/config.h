dc8db6d34a560df07a0f5f8ff0c16b6dc80a5131
ai6ryo-ship-it <ai.6.ryo@gmail.com>
2026-06-27 21:30:04 +0900

    Update config.h

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/via2/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/via2/config.h
@@ -27,13 +27,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#    define RGBLIGHT_EFFECT_SNAKE
 //#    define RGBLIGHT_EFFECT_KNIGHT
 //#    define RGBLIGHT_EFFECT_CHRISTMAS
-#    define RGBLIGHT_EFFECT_STATIC_GRADIENT
+//#    define RGBLIGHT_EFFECT_STATIC_GRADIENT
 //#    define RGBLIGHT_EFFECT_RGB_TEST
 //#    define RGBLIGHT_EFFECT_ALTERNATING
 //#    define RGBLIGHT_EFFECT_TWINKLE
 #endif
 
 #define TAP_CODE_DELAY 9
-
-#define POINTING_DEVICE_AUTO_MOUSE_ENABLE
-#define AUTO_MOUSE_DEFAULT_LAYER 1
+#define OLED_TIMEOUT 300000 // ミリ秒
+#define KEYBALL_CPI_DEFAULT 500 // マウス速度 (default: 500)
+#define KEYBALL_SCROLL_DIV_DEFAULT 4 // スクロール速度 (default: 4)
