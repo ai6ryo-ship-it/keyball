2775032665fba278ded4f41617d27c71654ee71c
ai6ryo-ship-it <ai.6.ryo@gmail.com>
2026-06-27 22:43:31 +0900

    Update config.h

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/via2/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/via2/config.h
@@ -39,6 +39,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define KEYBALL_CPI_DEFAULT 600 // マウス速度 (default: 500)
 #define KEYBALL_SCROLL_DIV_DEFAULT 4 // スクロール速度 (default: 4)
 
+void keyball_oled_render_cat(void); // <- これを追加
+
+
 #define LAYER_LED_ENABLE
 
 
